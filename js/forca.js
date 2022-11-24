e80040148ae18a9a0bd19e72252b4ace1789523c
isabela lacerda <lacerdaisa2017@gmail.com>
2022-11-24 12:01:22 -0300

    trabalho do tecnico

---
+++ js/forca.js
@@ -0,0 +1,302 @@
+
+const palavras=[
+    palavra002={
+    nome="EQUADOR"
+    categoria:"lugares"
+    }
+
+
+    palavra003={
+    nome="HUNGRIA"
+    categoria:"lugares"
+    }
+
+
+    palavra004={
+    nome="ETIOPIA"
+    categoria:"lugares"
+    }
+
+ 
+
+    palavra005={
+    nome="BRASIL"
+    categoria:"lugares"
+    }
+
+
+    palavra006={
+    nome="FRANÇA"
+    categoria:"lugares"
+    }
+
+
+    palavra007={
+    nome="ITALIA"
+    categoria:"lugares"
+    }
+
+    palavra008={
+    nome="VENEZUELA"
+    categoria:"lugares"
+    }
+
+    palavra009={
+    nome="MARROCOS"
+    categoria:"lugares"
+    }
+
+    palavra010={
+    nome="DINAMARCA"
+    categoria:"lugares"
+    }
+
+
+    palavra011={
+    nome="JARRA"
+    categoria:"objeto"
+    }
+ 
+    palavra012={
+    nome="MARTELO"
+    categoria:"objeto"
+    }
+
+    palavra013={
+    nome="JALECO"
+    categoria:"objeto"
+    }
+
+    palavra014={
+    nome="CAMISA"
+    categoria:"objeto"
+    }
+
+    palavra015={
+    nome="CANETA"
+    categoria:"objeto"
+    }
+
+
+    palavra016={
+    nome="TIARA"
+    categoria:"objeto"
+    }
+
+    palavra017={
+    nome="VASSOURA"
+    categoria:"objeto"
+    }
+
+    palavra018={
+    nome="CASACO"
+    categoria:"objeto"
+    }
+
+    palavra019={
+    nome="BLUSA"
+    categoria:"objeto"
+    }
+
+    palavra020={
+    nome="NOTBOOK"
+    categoria:"objeto"
+    }
+
+    palavra021={
+    nome="NIKE"
+    categoria:"marca"
+    }
+
+    palavra022={
+    nome="HAVAN"
+    categoria:"marca"
+    }
+
+    palavra023={
+    nome="RED BULL"
+    categoria:"marca"
+    }
+
+    palavra024={
+    nome="ADIDAS"
+    categoria:"marca"
+    }
+
+    palavra025={
+    nome="PUMA"
+    categoria:"marca"
+    }
+
+    palavra026={
+    nome="COCA-COLA"
+    categoria:"marca"
+    }
+
+    palavra027={
+    nome="MONSTER"
+    categoria:"marca"
+    }
+
+    palavra028={
+    nome="VANS"
+    categoria:"marca"
+    }
+
+    palavra029={
+    nome="PIRAQUE"
+    categoria:"marca"
+    }
+
+    palavra030={
+    nome="GUCCI"
+    categoria:"marca"
+    }
+
+    palavra031={
+    nome="Marcha Sobre Washington"
+    categoria:"manifestações históricas"
+    }
+
+    palavra032={
+    nome="Diretas Já"
+    categoria:"manifestações históricas"
+    }
+
+    palavra033={
+    nome="Black Lives Matter"
+    categoria:"manifestações históricas"
+    }
+
+
+
+    palavra034={
+    nome="MOVIMENTO ELE NÃO"
+    categoria:"manifestações históricas"
+    
+    }
+
+
+    palavra035={
+    nome="CARAS-PINTADAS"
+    categoria:"manifestações históricas"
+    
+    }
+
+
+    palavra036{
+    nome="COMÍCIO DA CANDELÁRIA"
+    categoria:"manifestações históricas"
+    
+    }
+
+
+    palavra037={
+    nome="NOVEMBRADA"
+    categoria:"manifestações históricas"
+    
+    }
+
+
+    palavra038={
+    nome="GREVE DA MEIA PASSAGEM"
+    categoria:"manifestações históricas"
+    
+    }
+
+
+
+    palavra039={
+    nome="SEXTA-FEIRA SANGRENTA"
+    categoria:"manifestações históricas"
+    
+    }
+
+
+    palavra040={
+    nome="PASSEATA DOS CEM MIL"
+    categoria:"manifestações históricas"
+    
+    }
+
+
+
+    palavra041={
+    nome="MAÇÃ"
+    categoria:"fruta"
+    
+    }
+
+
+    palavra042={
+    nome="BANANA"
+    categoria:"fruta"
+    }
+
+
+ 
+
+    palavra043={
+    nome="FRAMBOESA"
+    categoria:"fruta"
+    
+    }
+
+
+    palavra044={
+    nome="MORANGO"
+    categoria:"fruta"
+    
+    }
+
+
+    palavra045={
+    nome="CEREJA"
+    categoria:"fruta"
+    
+    }
+
+
+    palavra046={
+    nome="LARANJA"
+    categoria:"fruta"
+    
+    }
+
+
+    palavra047={
+    nome="PITAYA"
+    categoria:"fruta"
+    
+    }
+
+
+    palavra048={
+    nome="AMORA"
+    categoria:"fruta"
+    
+    }
+
+
+    palavra049={
+    nome="AMEIXA"
+    categoria:"fruta"
+    
+    }
+
+
+    palavra050={
+    nome="MIRTILO"
+    categoria:"fruta"
+    
+    }
+
+]
+
+function criarPalavraSecreta()[
+    const indexPalavra=(Math.random()*palavras.length)
+    console.log(indexPalavra)
+        
+]
+criarPalavraSecreta();
+
